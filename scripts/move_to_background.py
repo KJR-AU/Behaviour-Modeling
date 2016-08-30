d208e95e3b52e9d403092858cc8451785985307e
mjpedersen <mark.pedersen@kjross.com.au>
2016-08-30 10:23:27 +1000

    fixes to regen and background

--- scripts/move_to_background.py
+++ scripts/move_to_background.py
@@ -1,9 +1,9 @@
 #move selected node to background
 
 #TODO
-# load ast for the feature in which nodes are being merged 
+# load ast for the feature in which nodes are being merged
 # within the AST, change the merged nodes to comments
-# regenerate the feature file (maybe this is done on demand in another script) 
+# regenerate the feature file (maybe this is done on demand in another script)
 # separate feature to promote a node to the background
 
 import os
@@ -13,7 +13,7 @@ sys.path.append("/Library/Python/2.7/site-packages")
 from javax.swing import Box, BoxLayout, JLabel, JCheckBox, JComboBox, JTextField
 
 document.orientation = Application.ORIENTATION_TOP_TO_BOTTOM
-document.bias = Application.BIAS_START 
+document.bias = Application.BIAS_START
 document.addEntityAsSuccessor = True
 
 
@@ -23,90 +23,105 @@ merged=[]
 #iterate through the selection - keep first node and remove all others, transferring edges to the first node
 #print 'selected:', len(document.selection)
 if len(document.selection) < 1 :
-       Application.alert('You must select at least one node to move into the background.')
+    Application.alert('You must select at least one node to move into the background.')
 
 featureFileName = None
 feature = None
 
 for ge in document.selection:
     if ge.isEntity:
-       if count == 0:
-          root_node = ge
-          #get the filename of the feature for this node
-          featureFileName = ge.user['filename']
-          baseName = os.path.basename(featureFileName)
-          dirName = os.path.dirname(featureFileName)
-          pickleBaseName = os.path.splitext(baseName)[0] + '.pickle'
-          pickleFilePath = os.path.join(dirName,pickleBaseName)
-          #load the pickled AST for this feature
-          feature = pickle.load(open(pickleFilePath, "rb"))
-          #print feature
-       #find the matching AST node in the feature 
-       #where the AST node matches on the location line
-       #print feature['feature']['children'], '\n\n'
-       backgroundExists = False
-       for key,value in feature['feature'].items():
-	   if (key == 'children'):
-                   current_child = 0
-		   for child in value:
-                       print current_child
-		       if child['type'] == 'Background':
-			  backgroundExists = True
-                          backgroundIndex = current_child
-		          print child
-                          print feature['feature']['children'][backgroundIndex]
-			  #find last line of background scenario
-			  #find the Background group in the graph
-		       for step in child['steps']:
-			   print step 
-			   if step['location']['line'] == ge.user['line']:
-			       #print "AST node:", step['text']
-			       #print "Graph Element:", ge.title
-			       #copy it to a new comment item
-			       newBackgroundItem = step 
-			       #remove the original item from the feature section of the AST
-			       #child['steps'].remove(step)
-			       #insert the step into the background
-			       if backgroundExists:
-				  #update the line location of the new background clause and then append to background AST entry
-                                  newBackgroundItem['location']['line']= feature['feature']['children'][backgroundIndex]['location']['line'] + len(feature['feature']['children'][backgroundIndex]['steps']) + 1
-                                  feature['feature']['children'][backgroundIndex]['steps'].append(newBackgroundItem)
-                                  print "appended background:", feature['feature']['children'][backgroundIndex]
-				  #make newBackgroundItem node a child of the Background group
-                                  backgroundGroup = document.find("Background", None)[0]
-                                  matchingNodes = document.find(step['text'], None)
-                                  document.modifyAttribute(matchingNodes,"parent",backgroundGroup)
-				  #update all locations
-				  #iterate through AST, incrementing all location lines after the insert point by 1
-			       else:
-				  #insert Background after the Feature heading
-				  #create Background group in the graph
-				  newBackground = document.newGroup(None)[0] # no need to clearSelection each iteration
-				  newBackground.title = 'Background'
-				  #append newBackgroundItem to Background steps
-				  #update all locations
-				  #iterate through AST, incrementing all location lines after the insert point by 2
-                   current_child = current_child +1
+        if count == 0:
+            root_node = ge
+            #get the filename of the feature for this node
+            featureFileName = ge.user['filename']
+            baseName = os.path.basename(featureFileName)
+            dirName = os.path.dirname(featureFileName)
+            pickleBaseName = os.path.splitext(baseName)[0] + '.pickle'
+            pickleFilePath = os.path.join(dirName,pickleBaseName)
+            #load the pickled AST for this feature
+            feature = pickle.load(open(pickleFilePath, "rb"))
+            #print feature
+        #find the matching AST node in the feature
+        #where the AST node matches on the location line
+        #print feature['feature']['children'], '\n\n'
+        backgroundExists = False
+        for key,value in feature['feature'].items():
+            if (key == 'children'):
+                current_child = 0
+                for child in value:
+                    #print current_child
+                    if child['type'] == 'Background':
+                        backgroundExists = True
+                        backgroundIndex = current_child
+                        print child, feature['feature']['children'][backgroundIndex]
+                           #find last line of background scenario
+                            #find the Background group in the graph
+                    for step in child['steps']:
+                        #print step
+                        if step['location']['line'] == ge.user['line']:
+                            #print "AST node:", step['text']
+                            #print "Graph Element:", ge.title
+                            #copy it to a new comment item
+                            newBackgroundItem = step
+                            #remove the original item from the feature section of the AST
+                            #child['steps'].remove(step)
+                #insert the step into the background
+                if backgroundExists:
+                    #update the line location of the new background clause and then append to background AST entry
+                    newBackgroundItem['location']['line']= feature['feature']['children'][backgroundIndex]['location']['line'] + len(feature['feature']['children'][backgroundIndex]['steps']) + 1
+                    feature['feature']['children'][backgroundIndex]['steps'].append(newBackgroundItem)
+                    print "appended background:", feature['feature']['children'][backgroundIndex]
+                    #make newBackgroundItem node a child of the Background group
+                    backgroundGroup = document.find("Background", None)[0]
+                    document.modifyAttribute([ge],"parent",backgroundGroup)
+                    #update all locations
+                    #iterate through AST, incrementing all location lines after the insert point by 1
+                else:
+                    print "Creating Background"
+                    #find location of end of Feature description
+                    #create new Background AST entry
+                    newBackground={'steps': [], 'keyword': u'Background', 'type': 'Background', 'location': {'column': 1, 'line': 5}, 'name': u''}
+                    backgroundIndex=0
+                    #update the line location of the new background clause and then append to background AST entry
+                    #newBackgroundItem['location']['line'] = feature['feature']['children'][backgroundIndex]['location']['line'] + len(feature['feature']['children'][backgroundIndex]['steps']) + 1
+                    #insert Background after the Feature heading
+                    #feature['feature']['children'].insert(backgroundIndex,newBackgroundItem)
+                    feature['feature']['children'].insert(backgroundIndex,newBackground)
+                    #adjust step location and add steps to new Background
+                    newBackgroundItem['location']['line']= feature['feature']['children'][backgroundIndex]['location']['line'] + len(feature['feature']['children'][backgroundIndex]['steps']) + 1
+                    feature['feature']['children'][backgroundIndex]['steps'].append(newBackgroundItem)
+                    print "appended background:", feature['feature']['children'][backgroundIndex]
+                    #create Background group in the graph
+                    backgroundGroup = document.newGroup(None)[0] # no need to clearSelection each iteration
+                    backgroundGroup.title = 'Background'
+                    #find feature group and make backgroundGroup a child
+                    featureGroup = (ge.parent).parent
+                    document.modifyAttribute([backgroundGroup],"parent",featureGroup)
+                    #make newBackgroundItem node a child of the Background group
+                    document.modifyAttribute([ge],"parent",backgroundGroup)
+                    #update all locations
+                    #iterate through AST, incrementing all location lines after the insert point by 2
+                current_child = current_child +1
 
-	   #update pickled AST
-	   #print 'After', feature
-	   #enable one level of undo for merge
-	   #if pickleFilPath exists, rename pickleFilePath to os.path.splitext(baseName)[0] + '.1'
-	   with open(pickleFilePath, 'w') as pickleHandle:
-	       pickle.dump(feature, pickleHandle)
+        #update pickled AST
+        #print 'After', feature
+        #enable one level of undo for merge
+        #if pickleFilPath exists, rename pickleFilePath to os.path.splitext(baseName)[0] + '.1'
+        with open(pickleFilePath, 'w') as pickleHandle:
+            pickle.dump(feature, pickleHandle)
 
-	   #update the graph 
-	   #make the selected nodes children of the Background group
-	   #reconnect any other incoming our outgoing edges? probably not - we want to show the connection from (last node) of background
-	   #merged.append(ge)
-	   #if ge.hasOutEdges:
-	   #    for e in ge.outEdges:
-	   #	     document.reconnect(e, Application.EDGE_TAIL, root_node)
-	   #if ge.hasInEdges:
-	   #    for e in ge.inEdges:
-           #          document.reconnect(e, Application.EDGE_HEAD, root_node)
-       count = count + 1
-print feature 
+        #update the graph
+        #make the selected nodes children of the Background group
+        #reconnect any other incoming our outgoing edges? probably not - we want to show the connection from (last node) of background
+        #merged.append(ge)
+        #if ge.hasOutEdges:
+        #    for e in ge.outEdges:
+        #	     document.reconnect(e, Application.EDGE_TAIL, root_node)
+        #if ge.hasInEdges:
+        #    for e in ge.inEdges:
+            #          document.reconnect(e, Application.EDGE_HEAD, root_node)
+        count = count + 1
+print feature
 document.clearSelection()
 #document.selection = merged
 #print document.selection
--- scripts/regenerate_feature.py
+++ scripts/regenerate_feature.py
@@ -1,7 +1,7 @@
 #regenerate feature files from AST
 
 #TODO
-# load ast for the feature being regenerated 
+# load ast for the feature being regenerated
 
 import os
 import pickle
@@ -32,54 +32,57 @@ pickleFilePath = os.path.join(dirName,pickleBaseName)
 #load the pickled AST for this feature
 feature = pickle.load(open(pickleFilePath, "rb"))
 #print feature
-#print "initialising output" 
+#print "initialising output"
 output = ['\n'] * (count+10)  #empty array of strings the same size as the input, plus a margin
 #iterate through the AST, building up the output text in order of line number
 for key,value in feature.items():
   if (key == 'comments'):
       for comment in value:
-	  #print comment['text'], comment['location']['line']
-	  output[comment['location']['line']] = comment['text']
+        #print comment['text'], comment['location']['line']
+        output[comment['location']['line']] = comment['text']
   elif (key == 'tag'):
       for tag in value:
-	  #print tag['name'] 
-	  output[tag['location']['line']] = tag['name']
+         #print tag['name']
+         output[tag['location']['line']] = tag['name']
   elif (key == 'feature'):
       #print value['location']['line'], 'Feature: ', value['name']
       output[value['location']['line']] = 'Feature: ' + value['name']
       for featurekey,featurevalue in value.items():
-	  #if (featurekey == 'name'):
-	  #   print 'Feature', featurevalue
-	  #   #output[featurevalue['location']['line']] = 'Feature: ' + featurevalue['text'],
-	  #elif (featurekey == 'children'):
-	  if (featurekey == 'children'):
-	     for child in featurevalue:
-		  #for tag in child['tags']:
-		     #print tag['location']['line'], tag['name']
-		  #   output[tag['location']['line']] = tag['name']
-		  if child['type'] == 'Background':
-		      #print 'Background'
-		      output[child['location']['line']] = 'Background: '
-		  if child['type'] == 'Scenario':
-		      #print 'Scenario', child['name']
-		      output[child['location']['line']] = 'Scenario: ' + child['name']
-		  if child['type'] == 'ScenarioOutline':
-		      #print 'Scenario Outline', child['name']
-		      output[child['location']['line']] = 'Scenario Outline: ' + child['name']
-		  for step in child['steps']:
-			 #print step['location']['line'], step['keyword'], step['text'], '\n'
-			 step_text = "%s%s" % (step['keyword'], step['text'])
-                         output[step['location']['line']] = step_text 
+      #if (featurekey == 'name'):
+      #   print 'Feature', featurevalue
+      #   #output[featurevalue['location']['line']] = 'Feature: ' + featurevalue['text'],
+      #elif (featurekey == 'children'):
+        if (featurekey == 'children'):
+            for child in featurevalue:
+            #for tag in child['tags']:
+                #print tag['location']['line'], tag['name']
+            #   output[tag['location']['line']] = tag['name']
+                if child['type'] == 'Background':
+                    #print 'Background'
+                    output[child['location']['line']] = 'Background: '
+                if child['type'] == 'Scenario':
+                    #print 'Scenario', child['name']
+                    output[child['location']['line']] = 'Scenario: ' + child['name']
+                if child['type'] == 'ScenarioOutline':
+                    #print 'Scenario Outline', child['name']
+                    output[child['location']['line']] = 'Scenario Outline: ' + child['name']
+                for step in child['steps']:
+                    #print step['location']['line'], step['keyword'], step['text'], '\n'
+                    step_text = "%s%s" % (step['keyword'], step['text'])
+                    output[step['location']['line']] = step_text
 
 #with open(featureFileName, 'w') as pickleHandle:
 #      pickle.dump(feature, pickleHandle)
 
-#update feature file 
-myfile = open(featureFileName, 'w') 
+#update feature file
+myfile = open(featureFileName, 'w')
 line = 0
 for line in range(count+10):
     #print line, output[line]
-    myfile.write(output[line] + '\n')
+    if output[line] != '\n':
+        myfile.write(output[line] + '\n')
+    else:
+        myfile.write(output[line])
 myfile.close()
 
 document.clearSelection()
